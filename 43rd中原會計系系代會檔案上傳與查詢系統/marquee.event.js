f57e9781173ea02a4922d54137030fb3eb29a9a3
funwithme2021 <125599198+funwithme2021@users.noreply.github.com>
2023-11-20 12:51:53 +0800

    Add files via upload

--- 43rd中原會計系系代會檔案上傳與查詢系統/marquee.event.js
+++ 43rd中原會計系系代會檔案上傳與查詢系統/marquee.event.js
@@ -1,23 +1,31 @@
   // 跑马灯的文本内容
   const marqueeText = 
-  "◉歡迎來到中原大學會計系活動報名系統。 ◉致富導航器活動將於2023年12月06日(三)舉行。 ◉報名系統將於2023年11月20日12:00至2023年12月01日23:59開放報名。  ◉報名狀態查詢將於2023年12月04日00:00至2023年12月06日23:59開放查詢。";
+  "◉歡迎來到中原大學會計系活動報名系統。 ◉致富導航器活動將於2023年12月06日(三)舉行。 ◉企業參訪活動將於2023年12月15日(五)舉行。 ◉致富導航器活動報名系統將於2023年11月20日12:00至2023年12月01日23:59開放報名。 ◉企業參訪活動將於2023年12月15日(五)舉行。 ◉企業參訪活動報名系統將於2023年11月25日12:00至2023年12月01日23:59開放報名。  ◉報名狀態查詢將於2023年12月04日00:00至2023年12月06日23:59開放查詢。";
 
   const marqueeContentElement = document.getElementById('marquee-content');
   marqueeContentElement.textContent = marqueeText;
 
   document.addEventListener("DOMContentLoaded", function () {
     var signupButton = document.getElementById("signupButton");
+    var signupButton = document.getElementById("signup2Button");
     var statusButton = document.getElementById("statusButton");
 
     // 定義開放時間範圍
     var signupOpenTime = new Date("2023-11-20T12:00:00+08:00");
     var signupCloseTime = new Date("2023-12-01T23:59:00+08:00");
 
+    var signup2OpenTime = new Date("2023-11-25T12:00:00+08:00");
+    var signup2CloseTime = new Date("2023-12-01T23:59:00+08:00");
+
     var statusOpenTime = new Date("2023-12-04T00:00:00+08:00");
     var statusCloseTime = new Date("2023-12-06T23:59:00+08:00");
 
     signupButton.addEventListener("click", function () {
         checkOpenTime(signupOpenTime, signupCloseTime);
+    });
+
+    signup2Button.addEventListener("click", function () {
+        checkOpenTime(signup2OpenTime, signup2CloseTime);
     });
 
     statusButton.addEventListener("click", function () {
